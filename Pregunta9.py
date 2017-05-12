7d1f76b597d7949829d8b3aa57b0ac2f72089387
PraderioM <PraderioM@Gmail.com>
2017-05-12 16:14:13 +0200

    Add files via upload
    Added question 9

---
+++ Pregunta9.py
@@ -0,0 +1,92 @@
+from matplotlib import pyplot as plt
+from pylab import savefig
+from scipy.optimize import curve_fit
+from numpy import arange, inf
+from scipy import integrate
+from math import pi, exp
+
+rm=2**(1./6)
+u=lambda r: 4*((1/r)**12-(1/r)**6)
+e=abs(u(rm))
+B2T=lambda T: 2*pi*integrate.quad(lambda r: (1-exp(-u(r)/T))*r**2, 0, inf)[0]
+aT=lambda T: 2*pi*integrate.quad(lambda r: (1-exp(-(u(r)+e)/T))*r**2, 0, rm)[0]
+bT=lambda T: 2*pi*integrate.quad(lambda r: (1-(1-(u(r)+e)/T)*exp(-(u(r)+e)/T))*r**2, 0, rm)[0]
+
+f=lambda d: d*kT+B2*kT*d**2+(a*kT*d**2)*(8*(8-b*d)/(4-b*d)**3-1)
+g=lambda kT, B2, a, b: d*kT+B2*kT*d**2+(a*kT*d**2)*(8*(8-b*d)/(4-b*d)**3-1)
+
+D=8*[[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]]
+T=8*[[0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3]]
+P=[]
+errP=[]
+Colors=['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'gray']
+
+P.append([-0.001,-0.041,-0.114,-0.228,-0.359,-0.594,-1.022,-1.084])
+P.append([-0.003,-0.002,-0.11,-0.187,-0.401,-0.538,-0.969,-0.504])
+P.append([0.01,-0.028,-0.082,-0.192,-0.3,-0.525,-0.664,0.012])
+P.append([0.025,-0.005,-0.053,-0.142,-0.252,-0.45,-0.321,0.523])
+P.append([0.047,0.026,-0.018,-0.078,-0.203,-0.277,0.014,1.001])
+P.append([0.063,0.054,0.021,-0.031,-0.11,-0.062,0.351,1.486])
+P.append([0.077,0.091,0.068,0.033,0.018,0.143,0.678,1.949])
+P.append([0.091,0.122,0.119,0.116,0.143,0.352,0.973,2.406])
+
+errP.append([0.016,0.039,0.061,0.084,0.12,0.153,0.159,0.143])
+errP.append([0.016,0.016,0.061,0.089,0.11,0.154,0.147,0.161])
+errP.append([0.015,0.036,0.063,0.078,0.121,0.163,0.142,0.171])
+errP.append([0.016,0.035,0.061,0.083,0.102,0.129,0.153,0.184])
+errP.append([0.012,0.032,0.059,0.082,0.103,0.127,0.161,0.206])
+errP.append([0.01,0.031,0.054,0.082,0.106,0.144,0.173,0.218])
+errP.append([0.01,0.029,0.053,0.08,0.108,0.145,0.188,0.233])
+errP.append([0.011,0.03,0.055,0.084,0.117,0.153,0.196,0.239])
+
+P1=[[ P[i][j] for i in range(8)] for j in range(8)]
+errP1=[[ errP[i][j] for i in range(8)] for j in range(8)]
+
+
+for i in range(8):
+	plt.errorbar(D[i],P[i], yerr=errP[i], fmt='o', color=Colors[i])
+
+plt.xlabel('densidad reducida')
+plt.ylabel('presion reducida')
+
+curvas=[]
+Label=['T=0.6', 'T=0.7', 'T=0.8', 'T=0.9', 'T=1.0', 'T=1.1', 'T=1.2', 'T=1.3']
+#N=[7.4, 9, 10.4, 11, 11.4, 14.6, 8.5, 10]
+#b=[6.4, 9, 12, 13, 13.6, 17.7, 10.3, 12.2]
+#a=[-80, -98, -120, -115, -86, -65, -8, 15]
+X=arange(D[0][0], D[0][-1], 0.01)
+j=0
+a=5.1
+b=1.04
+for i in range(8):
+	kT=T[0][i]
+	B2=B2T(kT)
+	a=aT(kT)
+	b=bT(kT)
+	aux, =plt.plot(X, [f(d) for d in X], label=Label[i], color=Colors[i])
+	curvas.append(aux)
+
+plt.legend(curvas, Label, loc=2)
+plt.savefig('Isotermas.png')
+plt.clf()
+
+
+
+for i in range(8):
+	plt.errorbar(T[i],P1[i], yerr=errP1[i], fmt='o', color=Colors[i])
+
+plt.xlabel('temperatura reducida')
+plt.ylabel('presion reducida')
+
+curvas=[]
+Label=['p=0.1', 'p=0.2', 'p=0.3', 'p=0.4', 'p=0.5', 'p=0.6', 'p=0.7', 'p=0.8']
+X=arange(T[0][0], T[0][-1], 0.01)
+j=0
+for i in range(8):
+	d=D[0][i]
+	aux, =plt.plot(X, [g(kT, B2T(kT), aT(kT), bT(kT)) for kT in X], label=Label[i], color=Colors[i])
+	curvas.append(aux)
+
+plt.legend(curvas, Label, loc=2)
+plt.savefig('Isodensas.png')
+plt.clf()
